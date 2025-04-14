42008d5ce93874d988dfa55b9eaae72dbec09c50
EthanZiv <ethanzivagbayani@gmail.com>
2025-04-14 19:15:35 +0800

    tEST

--- resources/views/tickets/index.blade.php
+++ resources/views/tickets/index.blade.php
@@ -7,11 +7,6 @@
                     <h3 class="text-xl font-semibold">Thank You for Your Submission!</h3>
                 </div>
                 <div class="p-8 text-center">
-                    <div class="mb-4">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-16 w-16 text-green-500 mx-auto" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
-                        </svg>
-                    </div>
                     <h4 class="text-2xl font-bold text-gray-800 mb-2">Your ticket has been submitted!</h4>
                     <p class="text-gray-600 mb-6">We'll contact you shortly regarding your request!</p>
 
